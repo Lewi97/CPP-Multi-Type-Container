629b6044d25b8c14170860501657ef0369ea441d
Lewi97 <86553904+Lewi97@users.noreply.github.com>
2022-12-12 17:22:48 +0100

    Overhauled everything to fit my newer tastes

--- multitypecont.hpp
+++ multitypecont.hpp
@@ -1,48 +1,65 @@
-#ifndef MULTITYPECONTAINER_H
-#define MULTITYPECONTAINER_H
+#pragma once
 
 #include <vector>
-#include <unordered_map>
 #include <memory>
-#include "tools/constexpr-typehashing.hpp"
 
-struct Base {};
-
-template<typename T, class Container = std::vector<T>>
-struct Cont : public Base
+namespace lw
 {
-    using Container_t = Container;
+	class MultiTypeVector
+	{
+	public:
+		template<typename _Type>
+		using Pointer = std::unique_ptr<_Type>;
+	protected:
+		struct Base { virtual ~Base() {} };
 
-    Container_t _data;
-    auto& get() { return _data; }
+		template<typename _Type>
+		struct Data : Base { std::vector<_Type> data; };
 
-    void push_back(T val) { _data.push_back(val); }
-};
+		size_t _max_idx{ 0 };
+		std::vector<Pointer<Base>> _data;
 
-// class that can hold vectors of multiple types
-class MultiTypeContainer
-{
-protected:
-    std::unordered_map<size_t, std::shared_ptr<Base>> _vectors;
+		template <typename _Type>
+		struct UID
+		{
+			inline static size_t idx{ 0 };
+		};
 
-    template<typename T> struct hash_t final { inline static const size_t value { typehash::hash_code<T>() }; };
-public:
-    // will attempt to make a new vector of type T if T does not exist yet
-    template<typename T> void add(T val)
-    {
-        const auto h = hash_t<T>::value;       
+		template<typename _Type>
+		constexpr auto uid() { return UID<_Type>::idx; }
 
-        if (_vectors.find(h) == _vectors.end())
-            _vectors.insert({h, std::make_shared<Cont<T>>()});
-        
-        std::static_pointer_cast<Cont<T>>(_vectors[h])->push_back(val);
-    }
+		template<typename _Type>
+		constexpr auto
+			check_size()
+		{
+			constexpr auto& idx = UID<_Type>::idx;
+			if (idx == 0)
+			{
+				idx = _max_idx++;
+				_data.resize(_max_idx);
+				_data[idx] = std::make_unique<Data<_Type>>();
+			}
+		}
+	public:
+		MultiTypeVector() = default;
 
-    // returns: std::vector<T>&
-    template<typename T> auto& get()
-    {
-        return std::static_pointer_cast<Cont<T>>(_vectors[hash_t<T>::value])->get();
-    }
-};
+		template<typename _Type>
+		constexpr auto
+			push_back(_Type item)
+		{
+			check_size<_Type>();
+			get<_Type>().push_back(item);
+		}
 
-#endif // MULTITYPECONTAINER_H
+		template<typename _Type, typename... _Args>
+		constexpr decltype(auto)
+			emplace_back(_Args&&... args)
+		{
+			check_size<_Type>();
+			get<_Type>().emplace_back(std::forward<_Args>(args)...);
+		}
+
+		template<typename _Type>
+		constexpr auto& get() { return dynamic_cast<Data<_Type>*>(_data[uid<_Type>()].get())->data; }
+	};
+}
