eb555b5b6b5433e182a3ade227efd2e055abc506
Lewi97 <86553904+Lewi97@users.noreply.github.com>
2022-12-12 19:13:12 +0100

    Added support for returning multiple types

--- MultiTypeContainer.hpp
+++ MultiTypeContainer.hpp
@@ -1,30 +1,32 @@
 #pragma once
 
 #include <vector>
+#include <tuple>
 #include <memory>
 
 namespace lw
 {
 	class MultiTypeVector
 	{
-	public:
+	public: /* Public internal types */
 		template<typename _Type>
 		using Pointer = std::unique_ptr<_Type>;
-	protected:
+	protected: /* Protected internal types */
 		struct Base { virtual ~Base() {} };
 
 		template<typename _Type>
 		struct Data : Base { std::vector<_Type> data; };
 
-		size_t _max_idx{ 0 };
-		std::vector<Pointer<Base>> _data;
-
 		template <typename _Type>
 		struct UID
 		{
 			inline static size_t idx{ 0 };
-		};
-
+			inline static bool idx_set{ false };
+		};	
+	protected: /* Protected members */
+		size_t _max_idx{ 0 };
+		std::vector<Pointer<Base>> _data;
+	protected: /* Protected functions */
 		template<typename _Type>
 		constexpr auto uid() { return UID<_Type>::idx; }
 
@@ -33,14 +35,16 @@ namespace lw
 			check_size()
 		{
 			constexpr auto& idx = UID<_Type>::idx;
-			if (idx == 0)
-			{
-				idx = _max_idx++;
-				_data.resize(_max_idx);
-				_data[idx] = std::make_unique<Data<_Type>>();
-			}
+			if (UID<_Type>::idx_set) return;
+			idx = _max_idx++;
+			_data.resize(_max_idx);
+			_data[idx] = std::make_unique<Data<_Type>>();
+			UID<_Type>::idx_set = true;
 		}
-	public:
+
+		template<typename _Type>
+		constexpr auto& get_container() { return dynamic_cast<Data<_Type>*>(_data[uid<_Type>()].get())->data; }
+	public: /* Public functions */
 		MultiTypeVector() = default;
 
 		template<typename _Type>
@@ -59,7 +63,14 @@ namespace lw
 			get<_Type>().emplace_back(std::forward<_Args>(args)...);
 		}
 
-		template<typename _Type>
-		constexpr auto& get() { return dynamic_cast<Data<_Type>*>(_data[uid<_Type>()].get())->data; }
+		template<typename... _Types>
+		constexpr decltype(auto)
+			get() 
+		{
+			if constexpr (sizeof...(_Types) > 1)
+				return std::tuple<std::vector<_Types>&...>{ get_container<_Types>()... };
+			else
+				return get_container<_Types...>();
+		}
 	};
 }
